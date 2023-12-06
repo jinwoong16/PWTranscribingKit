f6f9d851d497b4dadb47dc32f69e2ec59993f97a
jinwoong <baseball0020@gmail.com>
2023-12-06 14:55:25 +0900

    Initial Commit

---
+++ PWTranscribingKit/PWTranscribingKit.h
@@ -0,0 +1,18 @@
+//
+//  PWTranscribingKit.h
+//  PWTranscribingKit
+//
+//  Created by jinwoong Kim on 12/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PWTranscribingKit.
+FOUNDATION_EXPORT double PWTranscribingKitVersionNumber;
+
+//! Project version string for PWTranscribingKit.
+FOUNDATION_EXPORT const unsigned char PWTranscribingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWTranscribingKit/PublicHeader.h>
+
+
